e363e881bf3cd88264864e763838586d823b3a00
Sumit Patil <sgpatil.2803@gmail.com>
2020-04-23 11:53:07 +0530

    Updates

---
+++ precache-manifest.08f84c0956b87feeb04fcaa1f03fd15a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fefc98eb219768a60105281f44adf1b",
+    "url": "/canvaro-job-listing/index.html"
+  },
+  {
+    "revision": "1938e5ebfcb35f3f7847",
+    "url": "/canvaro-job-listing/static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "5b1c1d2d7b268a90633c",
+    "url": "/canvaro-job-listing/static/js/2.a164cb4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvaro-job-listing/static/js/2.a164cb4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1938e5ebfcb35f3f7847",
+    "url": "/canvaro-job-listing/static/js/main.3a444b8f.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/canvaro-job-listing/static/js/main.3a444b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c88647a0e0b83c997b7",
+    "url": "/canvaro-job-listing/static/js/runtime-main.939e11f9.js"
+  }
+]);
