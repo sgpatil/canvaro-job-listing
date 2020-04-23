4fb018315d4d2f15a975feae9a79098c25dfd3ff
Sumit Patil <sgpatil.2803@gmail.com>
2020-04-23 11:55:28 +0530

    Basic component to show modal is created

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,98 @@
-import React from 'react'
-
-import { ExampleComponent } from 'canvaro-job-listing'
+import { JobListing } from 'canvaro-job-listing'
 import 'canvaro-job-listing/dist/index.css'
+import React, { Component } from "react";
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "dee.aye.en",
+      search: ""
+    };
+  }
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "70px"
+        }}
+      >
+        <h2 style={{}}>{"<JobListing />"}</h2>
+        <h5 style={{ color: "gray", marginBottom: "50px", marginTop: 0 }}>
+          npm i react-medium-profile
+        </h5>
+
+        <JobListing
+          username={
+            this.state.search.length > 0
+              ? this.state.search
+              : this.state.username
+          }
+        />
+        <h1 style={{ margin: 0, paddingTop: "30px" }}>
+          <span role="img" aria-label="pointing up">
+            👆
+          </span>
+        </h1>
+        <h5
+          style={{
+            textAlign: "center",
+            margin: 0,
+            paddingTop: "5px"
+          }}
+          onClick={() => this.setState({ username: "treyhuffine" })}
+        >
+          Click to fetch Medium profile
+        </h5>
+        <h6 style={{ margin: 0, paddingTop: "10px", color: "gray" }}>or</h6>
+        <input
+          style={{
+            marginTop: "10px",
+            border: "none",
+            marginBottom: this.state.search.length > 0 ? 0 : "40px",
+            padding: "3px",
+            textAlign: "center",
+            borderBottom: "1px solid black"
+          }}
+          type="text"
+          value={this.state.search}
+          onChange={e => this.setState({ search: e.target.value })}
+          placeholder="Find a profile"
+        ></input>
+        {this.state.search.length > 0 && (
+          <h6
+            style={{
+              marginTop: 0,
+              marginBottom: "40px",
+              paddingTop: "5px",
+              color: "gray"
+            }}
+          >
+            Now click the Medium button
+          </h6>
+        )}
+        <div
+          style={{
+            borderTop: "0.2px solid lightgray",
+            borderBottom: "0.2px solid lightgray",
+            width: "100%"
+          }}
+        >
+          <h4>props</h4>
+          <div style={{ marginLeft: "15px" }}>
+            <h4>
+              username: string <span style={{ color: "gray" }}>required</span>
+            </h4>
+            <h4>
+              size: number <span style={{ color: "gray" }}>optional</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App
